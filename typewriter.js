313b2f03aced55b1ebf11d99a9403aa80471df52
jvarghe <vcs.inbox@outlook.com>
2023-03-21 20:46:24 +0000

    Added comments and cleaned up 'typewriter.js'.

--- typewriter.js
+++ typewriter.js
@@ -1,4 +1,4 @@
-/* CHALLENGE [MODULE 1, WEEK 4] 
+/* CHALLENGE: ANIMATED SENTENCE [MODULE 1, WEEK 4] 
  * 
  * In this activity, we are going to take a sentence and display it as an 
  * animation. The tricky part will be to stagger the appearance of characters
@@ -8,14 +8,15 @@
  */
 
 
+
 // This function prints a sentence to screen. It takes a callback function that
-// uses setTimeout() to stagger the appearance of the characters. 
+// uses setTimeout() to stagger the appearance of the characters.
 const typeWriter = function(printChar) {
 
-  // This is the sentence to be printed.
+  // This is an array containing the sentence to be printed.
   const sentence = "hello there from lighthouse labs";
   
-  // This is the delay. It starts at 0 ms.
+  // This is the delay variable. It starts at 0 ms.
   let delay = 0;
 
 
@@ -28,17 +29,27 @@ const typeWriter = function(printChar) {
     delay += 50;
   }
 
+  // After all the characters are printed out, use `console.log` to print out
+  // the newline character so that the command prompt appears on a new line.
   console.log("\n");
 }
 
 
+
+// This is a function that prints characters to console.
 const printChar = function(char, delay) {
   
+  // Call setTimeout and pass in a delay.
   setTimeout(() => {
+
+    // Unlike `console.log`, `process.stdout.write` does not print a new line 
+    // character after every call.
     process.stdout.write(char);
+
   }, delay);
 }
 
 
 
-typeWriter();
+// DRIVER CODE:
+typeWriter(printChar);
